4a8645b192bf1a572fe6a4c2fa38a9e36e24a708
DzaferovicAmar <a.dzaferovic@students.htl-leonding.ac.at>
2024-02-29 13:26:17 +0100

    added PetsAndFleas

---
+++ PetsAndFleas/starter/PetsAndFleas/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsAndFleas;
+public class Cat : Pet
+{
+    private bool _isOnTree;
+    public int TreesClimbed { get; private set; }
+    public Cat() :base() 
+    {
+              
+    }
+    public bool ClimbOnTree()
+    {
+        _isOnTree = true;
+        TreesClimbed++;
+        return _isOnTree;
+    }
+    public bool ClimbDown()
+    {
+        _isOnTree = false;
+        return _isOnTree;
+    }
+    public override string ToString()
+    {
+        return "";
+    }
+}
---
+++ PetsAndFleas/starter/PetsAndFleas/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsAndFleas;
+public class Pet
+{
+    public static int NextPetId { get; private set;}
+    public int PetId { get; }
+    public int RemainingBites { get; private set; }
+    public Pet()
+    {
+        NextPetId = PetId++;
+        RemainingBites = 100;
+    }
+    int GetBitten(int bites)
+    {
+        RemainingBites -= bites;
+        return RemainingBites;
+    }
+}
